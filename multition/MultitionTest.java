61afc403969ac771eee00541cffd2d6bf5700330
tuchengwu <38784758+tuchengwu@users.noreply.github.com>
2019-10-22 18:04:08 +0800

    Multition
    KFC

---
+++ multition/MultitionTest.java
@@ -0,0 +1,13 @@
+package multition;
+
+public class MultitionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tourist tom = new Tourist("tom");
+		tom.gotoKFC();
+		tom.gotoKFC(0);
+		tom.gotoKFC(1);
+	}
+
+}
