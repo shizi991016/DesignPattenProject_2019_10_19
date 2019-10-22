4f162d0a1c2487b102acb05dc6d5176f9d287ce8
shizi9 <971883270@qq.com>
2019-10-22 17:28:44 +0800

    Revert "ForCreateFolder_师"
    This reverts commit 53a821c7a1d357c40b688f107c5a5bb182275450.

--- Scene_BuyTicket/Class/src/FacadePattern/FacadePattern.java
+++
@@ -1,5 +0,0 @@
-package FacadePattern;
-
-public class FacadePattern {
-
-}
--- Scene_BuyTicket/Class/src/ProxyPattern/Tickets.java
+++
@@ -1,7 +0,0 @@
-package ProxyPattern;
-
-public interface Tickets {
-	public abstract void Validate();			//验证信息
-	public abstract void Select(String type);	//选择门票种类
-	public abstract void Pay();					//支付
-}
