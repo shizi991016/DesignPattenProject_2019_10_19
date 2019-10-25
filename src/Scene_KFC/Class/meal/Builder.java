3d923c86d73acf6d1ffa68cd97c951fa51cd5975
Le Yang <36768174+Easonrust@users.noreply.github.com>
2019-10-25 15:48:33 +0800

    add annotation

--- src/Scene_KFC/Class/meal/Builder.java
+++ src/Scene_KFC/Class/meal/Builder.java
@@ -1,5 +1,9 @@
 package meal;
-
+/*
+ *套餐的抽象建造者类，定义了一系列抽象方法
+ * 其中buildAPart1-3为建造A套餐所需的步骤
+ * buildBPart1-3为建造B套餐所需的步骤
+ */
 public abstract class Builder {
     public abstract void buildAPart1();
     public abstract void buildAPart2();
--- src/Scene_KFC/Class/product/ColdDrink.java
+++ src/Scene_KFC/Class/product/ColdDrink.java
@@ -2,7 +2,9 @@ package product;
 
 import pack.Bottle;
 import pack.Packing;
-
+/*
+ *冷饮抽象类，对食物和冷饮接口的实现
+ */
 public abstract class ColdDrink implements Item{
 
     @Override
