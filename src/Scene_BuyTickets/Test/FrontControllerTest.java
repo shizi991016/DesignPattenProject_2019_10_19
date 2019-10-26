84a0efeefa2141561f99ec2663f440a76dd8317d
cainiao66 <17717084193@163.com>
2019-10-27 00:12:05 +0800

    fix bugs

--- src/Scene_BuyTickets/Test/FrontControllerTest.java
+++ src/Scene_BuyTickets/Test/FrontControllerTest.java
@@ -5,7 +5,7 @@ public class FrontControllerTest {
     public void FrontController(){
         System.out.println("****************************************************************************");
         System.out.println("模式一 前端控制器模式(Front Controller)：");
-        System.out.println("相关类：Hotel HotelManagementView HotelReceptionView HotelDispatcher");
+        System.out.println("相关类：Hotel HotelManagementView HotelReceptionView HotelViewDispatcher");
         System.out.println("模式一 测试一 由控制器Hotel调用Dispatcher分配到HotelReceptionView");
         Hotel hotel = Hotel.getInstance();
         hotel.dispatchRequest("reception");
