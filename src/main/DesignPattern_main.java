0d7a85d33e5bbdb398e0e81f3217569aec3fbb49
cainiao66 <17717084193@163.com>
2019-11-01 18:09:30 +0800

    fix
    fix

--- src/main/DesignPattern_main.java
+++ src/main/DesignPattern_main.java
@@ -12,14 +12,19 @@ public class DesignPattern_main {
 	public static void main(String[] args) {
 //		// 模式一 前端控制器
 //		FrontControllerTest.frontControllerTest();
+
 //		// 模式二 简单工厂
 //		EasyFactoryTest.easyFactoryTest();
+
 //		// 模式三 过滤器模式
 //		FilterPatternTest.filterPatternTest();
+
 //		// 模式四 空对象模式
 //		NullObjectTest.nullObjectTest();
+
 //		// 模式五 状态模式
 //		StatePatternTest.statePatternTest();
+
 //		// 模式六 代理模式
 //		ProxyPatternTest.proxyPatternTest();
 //
