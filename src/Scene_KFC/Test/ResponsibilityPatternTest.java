918a120c13ae5ac43b7e4a157c4b7b173cf466d1
Le Yang <36768174+Easonrust@users.noreply.github.com>
2019-11-05 17:30:26 +0800

    change num

--- src/Scene_KFC/Test/ResponsibilityPatternTest.java
+++ src/Scene_KFC/Test/ResponsibilityPatternTest.java
@@ -18,7 +18,7 @@ public class ResponsibilityPatternTest {
         manager.setNext(director);
         //开始测试
         System.out.println("\n" +"----------------------------------------");
-        System.out.println("31.责任链模式(Chain of Responsibility)：");
+        System.out.println("27.责任链模式(Chain of Responsibility)：");
 //        System.out.println("模式k 责任链模式(Chain of Responsibility)：");
 //        System.out.println("相关类：Handler WaiterHandler ManagerHandler DirectorHandler");
         System.out.println("\n测试一:\n请求在责任链第一级即Waiter级完成");
