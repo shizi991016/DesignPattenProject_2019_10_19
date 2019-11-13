0f5f0fe6a9e7851cce7f60a391ee57de846db69e
cainiao66 <17717084193@163.com>
2019-11-13 08:48:58 +0800

    TEST-宁

--- TEST/SimpleFactoryPatternTest.java
+++ TEST/SimpleFactoryPatternTest.java
@@ -1,12 +1,15 @@
 package Test;
 
+import RestaurantScene.Composite.Menu;
 import RestaurantScene.Factory.SimpleFoodFactory;
 import RestaurantScene.Flyweight.FlyweightOrderFactory;
 
 public class SimpleFactoryPatternTest {
     public static void main(String []args){
         SimpleFoodFactory simpleFoodFactory = new SimpleFoodFactory();
-        simpleFoodFactory.createFood("12",123,"213");
-        simpleFoodFactory.createFood("12",123,"Pizza");
+        Menu menu = new Menu();
+        menu.addFood(simpleFoodFactory.createFood("12",123,"213"));
+        menu.addFood(simpleFoodFactory.createFood("12",123,"Pizza"));
+        menu.display();
     }
 }
